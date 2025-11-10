c05b3f97ed28bdf96ca7e9d9477d38c74da74fe2
turc1656 <34428745+turc1656@users.noreply.github.com>
2025-11-10 02:02:24 -0500

    chore: disable flask debug and bump ui version (missed from last commit)
    * Follow-up to previous RapidAPI integration commit
    * Disable Flask debug mode for production deployment
    * Update displayed UI version in index.html from v1.3.0 to v2.0.0
    * These adjustments were unintentionally omitted from the prior commit

--- app.py
+++ app.py
@@ -159,4 +159,4 @@ def fetch_transcript_endpoint():
 
 if __name__ == '__main__':
     load_dotenv()  # Load environment variables from .env file
-    app.run(host='0.0.0.0', port=5000, debug=True)  # TODO REMINDER: Remove debug=True when deploying (this is the auto-reload feature)
+    app.run(host='0.0.0.0', port=5000, debug=False)  # TODO REMINDER: Remove debug=True when deploying (this is the auto-reload feature)
